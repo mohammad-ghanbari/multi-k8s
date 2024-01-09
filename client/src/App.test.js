f6b183fece4b7a3ee96711f247dde8fa9a8d0099
Mohamad <mghanbari.mohamad@gmail.com>
2024-01-09 15:35:50 -0500

    added client folder

---
+++ client/src/App.test.js
@@ -0,0 +1,6 @@
+import { render, screen } from '@testing-library/react';
+import App from './App';
+import React from 'react';
+import { ReactDOM } from 'react';
+
+it('renders without crashing', () => {});
