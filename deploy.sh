a350315876cc4158178090705f082f5d6b5ef4b6
Mohamad <mghanbari.mohamad@gmail.com>
2024-01-11 10:52:52 -0500

    added deployment scripts

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+docker build -t mgh1990/multi-client:latest -t mgh1990/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t mgh1990/multi-server:latest -t mgh1990/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t mgh1990/multi-worker:latest -t mgh1990/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+
+docker push mgh1990/multi-client:latest
+docker push mgh1990/multi-server:latest
+docker push mgh1990/multi-worker:latest
+docker push mgh1990/multi-client:$SHA
+docker push mgh1990/multi-server:$SHA
+docker push mgh1990/multi-worker:$SHA
+
+kubectl apply -f k8s
+
+kubectl set image deployments/server-deployment server=mgh1990/multi-server:$SHA
+kubectl set image deployments/client-deployment server=mgh1990/multi-client:$SHA
+kubectl set image deployments/worker-deployment server=mgh1990/multi-worker:$SHA
